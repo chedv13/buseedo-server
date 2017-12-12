138db1b840cd391a7a04cfa2b433c8dcd336630f
Dmitry Chekurov <chedv13@gmail.com>
2017-12-12 23:20:56 +0400

    changed default AdminUser creation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@buseedo.com', password: '8X1Q419z4eaBnyE', password_confirmation: '8X1Q419z4eaBnyE') if Rails.env.development?
