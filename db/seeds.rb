2c1af1f863593ecb452bf9e95d05d975e32d7f7e
Dmitry Chekurov <chedv13@gmail.com>
2017-12-13 13:19:10 +0400

    fixed db:seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-AdminUser.create!(email: 'admin@buseedo.com', password: '8X1Q419z4eaBnyE', password_confirmation: '8X1Q419z4eaBnyE') if Rails.env.development?
+AdminUser.create!(email: 'admin@buseedo.com', password: '8X1Q419z4eaBnyE', password_confirmation: '8X1Q419z4eaBnyE')
