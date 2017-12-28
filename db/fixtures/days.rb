c4a778b5f761b7385bed170cb56f3a20640d18b2
Dmitry Chekurov <chedv13@gmail.com>
2017-12-28 19:39:45 +0400

    fixed fixtures.

--- db/fixtures/days.rb
+++ db/fixtures/days.rb
@@ -1,5 +1,5 @@
 # TODO: Здесь бы по хорошему применить реально сколько дней в году
-(0..365).each do |number|
+(1..365).each do |number|
   Day.seed do |d|
     d.id = number
     d.number = number
--- db/fixtures/tasks_and_day_tasks.rb
+++ db/fixtures/tasks_and_day_tasks.rb
@@ -2,18 +2,16 @@ require 'faker'
 
 number_of_points = 1
 Day.find_each do |day|
-  (0..2).each do |number|
-    number_of_points += 1
+  (1..3).each do |number|
     random_paragraphs_number = rand(1..4)
     tasks = Task.seed do |t|
       t.body = Faker::Lorem.paragraph(random_paragraphs_number)
       t.id = number_of_points
       t.number_of_points = number_of_points
     end
+    number_of_points += 1
     task = tasks[0]
-    if Task.count == 1
-      task.update_attribute(:is_default, true)
-    end
+    task.update_attribute(:is_default, true) if Task.count == 1
     DayTask.seed do |dt|
       dt.id = number_of_points
       dt.day_id = day.id
