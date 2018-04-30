8f119dc093f33a1eeeddd2886d2fae52f4ef9992
Dmitry Chekurov <chedv13@gmail.com>
2018-05-01 00:40:01 +0400

    updated admin resources.

--- app/admin/skills.rb
+++ app/admin/skills.rb
@@ -1,3 +1,5 @@
 ActiveAdmin.register Skill do
   filter :name, filters: [:contains]
+
+  permit_params :name
 end
--- app/admin/task.rb
+++ app/admin/task.rb
@@ -9,8 +9,10 @@ ActiveAdmin.register Task do
     id_column
     column :name
     column :body
-    column :is_default
+    column :is_published
+    column :serial_number
     column :number_of_points
+    column :number_of_percentages
     column 'Skills' do |task|
       %{
         <ul style="padding-left: 5px; list-style: disc;">
@@ -23,18 +25,40 @@ ActiveAdmin.register Task do
     actions
   end
 
-  permit_params :body, :name, :number_of_percentages, :number_of_points, skill_ids: []
+  permit_params :body, :is_published, :name, :number_of_percentages, :number_of_points, :serial_number, skill_ids: []
 
   form do |f|
     f.inputs 'Basic fields' do
       f.input :name
-      f.input :body
+      f.input :body, as: :froala_editor
+      f.input :is_published
+      f.input :serial_number
       f.input :number_of_points
       f.input :number_of_percentages
     end
     f.inputs 'Relationships' do
+      f.input :day, as: :select, collection: Day.all.map { |d| ["course: #{d.course.name} - day_number: #{d.number}", d.id] }, include_blank: false
       f.input :skills, as: :select, input_html: { multiple: true, size: 10 }
     end
     f.actions
+  end
+
+  show do |c|
+    attributes_table do
+      row :name
+      row :body do
+        c.body.html_safe
+      end
+      row :is_published
+      row :serial_number
+      row :number_of_points
+      row :number_of_percentages
+      row :day do
+        "course: #{c.day.course.name} - day_number: #{c.day.number}"
+      end
+      row :skills do
+        c.skills.map { |s| s.name }.to_s
+      end
+    end
   end
 end
