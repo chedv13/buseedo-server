662ddd75830e6f372574b2dbec6044071a0ee31c
Dmitry Chekurov <chedv13@gmail.com>
2018-07-30 12:35:07 +0400

    updated capistrano version.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.10.0'
+lock '~> 3.11.0'
 
 set :application, 'buseedo'
 set :repo_url, 'https://copadrop-deployer:kqoJOl6yz0G7K3A@github.com/chedv13/buseedo-server'
@@ -10,5 +10,5 @@ set :use_sudo, false
 set :rvm_ruby_version, '2.4.1'
 set :linked_dirs, fetch(:linked_dirs, []).push('public/system')
 set :default_env, {
-    :PASSENGER_INSTANCE_REGISTRY_DIR => '/tmp'
+  :PASSENGER_INSTANCE_REGISTRY_DIR => '/tmp'
 }
