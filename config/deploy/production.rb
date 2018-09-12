209b0453d3de43b822ecc2753bb860cef2f409bf
Evgeny <sacutaes@gmail.com>
2018-09-12 18:16:26 +0400

    change user for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,4 @@
-server '159.203.120.126', user: 'chedv13', roles: %w{app db web}
+#server '159.203.120.126', user: 'chedv13', roles: %w{app db web}
+server '159.203.120.126', user: 'sacuta', roles: %w{app db web}
 set :rails_env, 'production'
+
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,3 @@
-server '46.101.175.51', user: 'chedv13', roles: %w{app db web}
+#server '46.101.175.51', user: 'chedv13', roles: %w{app db web}
+server '46.101.175.51', user: 'sacuta', roles: %w{app db web}
 set :rails_env, 'staging'
